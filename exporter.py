8e5105d8ec7633a2220560c0d4ec56766c9650b5
Swann <swann.martinez@protonmail.com>
2023-03-17 18:58:59 +0100

    feat: monthly and daily power consumption

--- exporter.py
+++ exporter.py
@@ -2,13 +2,34 @@ import asyncio
 import os
 import time
 from aioprometheus.service import Service
-from aioprometheus import Gauge
-
+from aioprometheus import Gauge, Counter
+from datetime import datetime, date
 from meross_iot.http_api import MerossHttpClient
 from meross_iot.manager import MerossManager
 
 EMAIL = os.environ.get('MEROSS_EMAIL') or "YOUR_MEROSS_CLOUD_EMAIL"
 PASSWORD = os.environ.get('MEROSS_PASSWORD') or "YOUR_MEROSS_CLOUD_PASSWORD"
+
+def _get_today_consumption(daily_consumption_history):
+    today_time = datetime.now()
+    today_time = today_time.replace(hour=0, minute=0, second=0, microsecond=0)
+    today_consumption = 0
+    for daily_history in daily_consumption_history:
+        if daily_history['date'] == today_time:
+            today_consumption = daily_history['total_consumption_kwh']
+    return today_consumption
+
+
+def _get_monthly_consumption(daily_consumption_history):
+    current_month = datetime.today().month
+
+    month_daily_history = [h for h in daily_consumption_history if h['date'].month == current_month]
+    monthly_consumption = 0
+
+    for daily_history in month_daily_history:
+        monthly_consumption += daily_history['total_consumption_kwh']
+
+    return monthly_consumption
 
 
 async def main():
@@ -17,6 +38,8 @@ async def main():
     power = Gauge('meross_power', 'Electricity watt reading (w)')
     voltage = Gauge('meross_voltage', 'Electricity voltage reading (v)')
     current = Gauge('meross_current', 'Electricity ampere reading (a)')
+    daily_consumption = Gauge('meross_daily_consumption', 'Electricity daily consumption (w)')
+    monthly_consumption = Gauge('meross_monthly_consumption', 'Electricity monthly consumption (w)')
     await service.start(addr="0.0.0.0", port=8000)
     print(f"Serving prometheus metrics on: {service.metrics_url}")
 
@@ -34,7 +57,7 @@ async def main():
     if len(plugs) < 1:
         print("No MSS310 plugs found...")
     else:
-        async def updater(p: Gauge, v: Gauge, c: Gauge):
+        async def updater(p: Gauge, v: Gauge, c: Gauge, dc: Gauge, mc: Gauge):
             while True:
                 for dev in plugs:
                     try:
@@ -42,11 +65,16 @@ async def main():
                         p.set({'device': dev.name},instant_consumption.power)
                         v.set({'device': dev.name},instant_consumption.voltage)
                         c.set({'device': dev.name},instant_consumption.current)
+
+                        daily_consumption_history = await dev.async_get_daily_power_consumption()
+                        dc.set({'device': dev.name}, _get_today_consumption(daily_consumption_history))
+                        mc.set({'device': dev.name}, _get_monthly_consumption(daily_consumption_history))
+                        
                     except Exception as e:
                         print(f"Exception caught: {e}")
                 await asyncio.sleep(30.0)
 
-        await updater(power,voltage,current)
+        await updater(power,voltage,current, daily_consumption, monthly_consumption)
 
 
     # Close the manager and logout from http_api
